20dfc33668e2b93ad760be1ef06831f056960c54
jsy <jjungseoyeon@naver.com>
2021-08-25 16:24:58 +0900

    Initial commit

---
+++ src/main/java/com/sparta/navershop/NavershopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.navershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavershopApplication.class, args);
+    }
+
+}
