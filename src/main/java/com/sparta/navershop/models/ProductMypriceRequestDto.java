6cbbbb8f07990c8ea73666cbc1d80a8a48e4ee68
jsy <jjungseoyeon@naver.com>
2021-09-13 21:40:16 +0900

    Register product of interest

---
+++ src/main/java/com/sparta/navershop/models/ProductMypriceRequestDto.java
@@ -0,0 +1,8 @@
+package com.sparta.navershop.models;
+
+import lombok.Getter;
+
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
---
+++ src/main/java/com/sparta/navershop/models/ProductRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.navershop.models;
+
+import lombok.Getter;
+
+@Getter
+
+public class ProductRequestDto {
+//    title, link, image, lprice
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+}
