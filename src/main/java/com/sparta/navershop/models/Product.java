12d75f6281707b7903775ce66434ed464c7466dd
jsy <jjungseoyeon@naver.com>
2022-01-08 22:49:08 +0900

    Unit Test

--- src/main/java/com/sparta/navershop/models/Product.java
+++ src/main/java/com/sparta/navershop/models/Product.java
@@ -1,6 +1,7 @@
 package com.sparta.navershop.models;
 
 import com.sparta.navershop.dto.ProductRequestDto;
+import com.sparta.navershop.utils.URLValidator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -37,6 +38,27 @@ public class Product extends Timestamped{
 
     // 관심 상품 생성 시 이용합니다.
     public Product(ProductRequestDto requestDto, Long userId) {
+        // 입력값 Validation
+        if (userId == null || userId < 0) {
+            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+        }
+
+        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+        }
+
+        if (!URLValidator.urlValidator(requestDto.getImage())) {
+            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (!URLValidator.urlValidator(requestDto.getLink())) {
+            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (requestDto.getLprice() <= 0) {
+            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+        }
+
         // 관심상품을 등록한 회원 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
